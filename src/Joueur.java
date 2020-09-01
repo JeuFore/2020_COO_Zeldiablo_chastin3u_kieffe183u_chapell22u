e1360cd59947fc1f51a7b805cb1b9ba5940a7f82
kieffe183u <charlie.kieffer1@etu.univ-lorraine.fr>
2020-09-01 15:25:04 +0200

    Modification de la methode de deplacement du joueur

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,18 +1,31 @@
-public class Joueur{
+public class Joueur extends CClavier {
 
     private final int POINT_DE_VIE = 10;
 
     private int positionX;
     private int positionY;
+    private boolean collision;
 
     public Joueur(String nom, int x, int y) {
         this.positionX = x;
         this.positionY = y;
     }
 
-    public void move(int newX, int newY) {
-        this.positionX = newX;
-        this.positionY = newY;
+    public void move() {
+        if (!collision){
+            if (isPressed(90)){
+                this.positionY += 1;
+            }
+            if (isPressed(81)){
+                this.positionX -= 1;
+            }
+            if (isPressed(83)){
+                this.positionY -= 1;
+            }
+            if (isPressed(68)){
+                this.positionX += 1;
+            }
+        }
     }
 
     public int getPositionX() {
@@ -29,6 +42,10 @@ public class Joueur{
 
     public void setPositionY(int y) {
         this.positionY = y;
+    }
+
+    public int getVie(){
+        return POINT_DE_VIE;
     }
 
 }
