9c461ce3aeeab9cea7324fee34f01bba3a13c42d
FireKing54 <theo2001@laposte.net>
2020-09-02 09:50:17 +0200

    draft of Characters package

---
+++ src/code/characters/Character.java
@@ -0,0 +1,10 @@
+package code.characters;
+
+public abstract class Character {
+    
+    private String nom;
+    private int vie;
+    
+    private boolean collision;
+
+}
---
+++ src/code/characters/Position.java
@@ -0,0 +1,35 @@
+package code.characters;
+
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public Position() {
+        this.setX(0);
+        this.setY(0);
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    
+    
+}
