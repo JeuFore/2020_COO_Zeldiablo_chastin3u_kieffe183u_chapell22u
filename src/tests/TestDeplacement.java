94c8bedbc098f405733ee202394b1ac422eea4fe
kieffe183u <charlie.kieffer1@etu.univ-lorraine.fr>
2020-09-02 14:45:49 +0200

    Nouveau commit pour modifier la classe de test TestDeplacement, remplacement Joueur par Character

--- src/tests/TestDeplacement.java
+++ src/tests/TestDeplacement.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import code.Jeu;
 import code.characters.Advanturer;
+import code.characters.Character;
 
 import static org.junit.Assert.*;
 
@@ -26,27 +27,27 @@ public class TestDeplacement {
     
     @Test
     public void testDeplacementSurX(){
-        Joueur j = new Joueur("Bob", 1, 1);
+        Character j = new Advanturer("Bob",10, 1, 1);
         j.move(1, 0);
-        assertEquals("Le joueur j devrait etre a 2 1", 2, j.getPositionX());
-        assertEquals("Le joueur j devrait etre a 2 1", 1, j.getPositionY());
+        assertEquals("Le joueur j devrait etre a 2 1", 2, j.getPosition().getX());
+        assertEquals("Le joueur j devrait etre a 2 1", 1, j.getPosition().getY());
     }
 
     @Test
     public void testDeplacementSurY(){
-        Joueur j = new Joueur("Bob", 1, 1);
+    	Character j = new Advanturer("Bob",10, 1, 1);
         j.move(0, 1);
-        assertEquals("Le joueur j devrait etre a 1 2", 1, j.getPositionX());
-        assertEquals("Le joueur j devrait etre a 1 2", 2, j.getPositionY());
+        assertEquals("Le joueur j devrait etre a 1 2", 1, j.getPosition().getX());
+        assertEquals("Le joueur j devrait etre a 1 2", 2, j.getPosition().getY());
     }
 
     @Test
     public void testDeplacementCollision(){
-        Joueur j = new Joueur("Bob", 1, 1);
+    	Character j = new Advanturer("Bob",10, 1, 1);
         j.bloquer(true);
         j.move(0, 1);
-        assertEquals("Le joueur devrait ne pas avoir bouge", 1, j.getPositionX());
-        assertEquals("Le joueur devrait ne pas avoir bouge", 1, j.getPositionY());
+        assertEquals("Le joueur devrait ne pas avoir bouge", 1, j.getPosition().getX());
+        assertEquals("Le joueur devrait ne pas avoir bouge", 1, j.getPosition().getY());
     }
 
     @Test
